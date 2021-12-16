76b81e5b336539badb00cbcce0d2b49541d83bf3
李伟 <liwei@37.com.com>
2021-12-16 19:03:13 +0800

    fix compiler error

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor", from: "4.41.0"),
         /// feather core
-        .package(url: "https://github.com/tomlee130/feather-core", .branch("main")), //from: "1.0.0-beta"),
+        .package(url: "https://github.com/tomlee130/feather-core", .branch("main")),//from: "1.0.0-beta"),
         /// drivers
         .package(url: "https://github.com/vapor/fluent-sqlite-driver", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent-mysql-driver", from: "4.0.0"),
@@ -20,7 +20,7 @@ let package = Package(
         .package(url: "https://github.com/binarybirds/liquid-local-driver", from: "1.2.0"),
         /// modules
         .package(url: "https://github.com/tf-swufe-org/redirect-module", .branch("main")),//from: "1.0.0-beta"),
-        .package(url: "https://github.com/tomlee130/blog-module", .branch("main")),
+        .package(url: "https://github.com/tomlee130/blog-module", .branch("main")),//from: "1.0.0-beta"),
         .package(url: "https://github.com/tf-swufe-org/analytics-module", .branch("main")),//from: "1.0.0-beta"),
         .package(url: "https://github.com/tf-swufe-org/aggregator-module", .branch("main")),//from: "1.0.0-beta"),
         .package(url: "https://github.com/tf-swufe-org/sponsor-module", .branch("main")),//from: "1.0.0-beta"),
